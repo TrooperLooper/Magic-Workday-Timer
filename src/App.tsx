1ed7fe950600f352d2c412e4bbd4382a76d52824
TrooperLooper <lars@recap.dk>
2026-01-01 22:17:58 +0100

    Refactor: Use scale-based responsive design with wrapper divs

--- src/App.tsx
+++ src/App.tsx
@@ -20,24 +20,26 @@ export default function App(): React.ReactElement {
   } = usePomodoro();
 
   return (
-    <div>
-      <div className="timer-container">
-        <MinutesCircle
-          totalSeconds={currentTimer.minutes}
-          secondsLeft={timeLeft}
-          isRunning={isRunning}
-          timerType={getTimerType(currentTimer.minutes)}
-        />
-        <BigButtonDot
-          color={currentTimer.color}
-          isRunning={isRunning}
-          onClick={handleButtonClick}
-        >
-          <CountdownNumber value={timeLeft} />
-        </BigButtonDot>
+    <div className="app-wrapper">
+      <div className="timer-wrapper">
+        <div className="timer-container">
+          <MinutesCircle
+            totalSeconds={currentTimer.minutes}
+            secondsLeft={timeLeft}
+            isRunning={isRunning}
+            timerType={getTimerType(currentTimer.minutes)}
+          />
+          <BigButtonDot
+            color={currentTimer.color}
+            isRunning={isRunning}
+            onClick={handleButtonClick}
+          >
+            <CountdownNumber value={timeLeft} />
+          </BigButtonDot>
+        </div>
+        <PillRow completedSteps={completedSteps} />
+        <StarsRow completedSets={completedSets} />
       </div>
-      <PillRow completedSteps={completedSteps} />
-      <StarsRow completedSets={completedSets} />
     </div>
   );
 }
